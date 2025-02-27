298eccfcbfa72bcb540142ce0c296d613a60ebc2
KomalMavani <102403515+KomalMavani@users.noreply.github.com>
2025-02-27 13:42:25 -0500

    Animal Apis
    1. Get Animals List
    2. Update Animal
    3. Search Animals
    4. Get Animal Details

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const authRoutes = require('./src/routes/authRoutes');
 const errorHandler = require('./src/middleware/errorHandler');
 const petRoutes = require('./src/routes/petRoutes');
 const breedRoutes = require('./src/routes/breedRoutes');
+const animalRoutes = require('./src/routes/animalRoutes');
+const favoriteRoutes = require('./src/routes/favoriteRoutes');
+
 
 const app = express();
 app.use(express.json());
@@ -12,7 +15,10 @@ app.use('/uploads', express.static('uploads')); // Serve profile images
 app.use('/api/auth', authRoutes);
 app.use('/api', petRoutes);
 app.use('/api', breedRoutes);
+app.use('/api', animalRoutes);
+app.use('/api', favoriteRoutes);
+
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ backend/src/models/animalModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const AnimalSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    images: { type: [String], required: true },
+    gender: { type: String, enum: ["Male", "Female"], required: true },
+    petType: { type: mongoose.Schema.Types.ObjectId, ref: "Pet", required: true },
+    breedType: { type: mongoose.Schema.Types.ObjectId, ref: "Breed" },
+    size: { type: String, enum: ["Small", "Medium", "Large"], required: true },
+    age: { type: String, enum: ["Baby", "Young", "Adult", "Senior"], required: true },
+    location: {
+        lat: { type: Number, required: true },
+        lng: { type: Number, required: true }
+    },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    country: { type: String, required: true },
+    honourName: { type: String, required: true },
+    description: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Animal', AnimalSchema);
