6110fd69137b31f66ea1538dcd33f82a9e83a3af
KomalMavani <102403515+KomalMavani@users.noreply.github.com>
2025-02-27 13:40:15 -0500

    Authentication Middleware

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { sendOTP, verifyOTP,registerUser } = require('../controllers/authController');
+const { sendOTP, verifyOTP, registerUser, profileUser,updateProfile } = require('../controllers/authController');
+const { authMiddleware } = require('../middleware/authMiddleware');
+
 const router = express.Router();
 router.post('/send-otp', sendOTP);
 router.post('/verify-otp', verifyOTP);
 router.post('/register', registerUser);
+router.get('/profile', authMiddleware, profileUser);
+router.post('/profile/update', authMiddleware, updateProfile);
 
 module.exports = router;
