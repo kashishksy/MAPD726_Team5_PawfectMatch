86b35540de97828b6a39940ee0270a12e8692a44
kashishksy <kashishksy2112@gmail.com>
2025-02-26 22:00:58 -0500

    adding redux to app.tsx files

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,8 +1,15 @@
+//frontend/App.tsx
 import React from 'react';
+import { Provider } from 'react-redux'; // Step 1: Import Provider
+import { store } from './redux/store'; // Step 1: Import store
 import AppNavigator from './navigation/AppNavigator';
 
 function App(): React.JSX.Element {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}> {/* Step 2: Wrap AppNavigator with Provider */}
+      <AppNavigator />
+    </Provider>
+  );
 }
 
 export default App;
